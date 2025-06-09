8fbe9f84b53150a9e933690f3cd6967e4885362a
Atabek77-alt <atabekis2006@gmail.com>
2025-06-10 01:07:21 +0600

    end commit

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+from pydantic import BaseModel
+
+
+class Settings(BaseModel):
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST: str = os.getenv("DB_HOST", "db")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_NAME: str = os.getenv("DB_NAME", "tournaments_db")
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+settings = Settings()
